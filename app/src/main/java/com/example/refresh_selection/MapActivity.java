1f9d8c301adf9c201b889711d958ff310273a045
eunbin <sag06051@gmail.com>
2021-11-06 19:57:27 +0900

    Feature 매핑 파일 추가

--- app/src/main/java/com/example/refresh_selection/MapActivity.java
+++ app/src/main/java/com/example/refresh_selection/MapActivity.java
@@ -1,57 +1,84 @@
-//package com.example.refresh_selection;
-//
-//import android.content.pm.PackageInfo;
-//import android.content.pm.PackageManager;
-//import android.content.pm.Signature;
-//import android.os.Build;
-//import android.os.Bundle;
-//import android.util.Base64;
-//import android.util.Log;
-//import android.view.ViewGroup;
-//
-//import androidx.annotation.RequiresApi;
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import net.daum.mf.map.api.MapView;
-//
-//import java.security.MessageDigest;
-//import java.security.NoSuchAlgorithmException;
-//
-////import net.daum.android.map.MapView;
-//
-//public class MapActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_map);
-//
-//        MapView mapView = new MapView(this);
-//
-//        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
-//        mapViewContainer.addView(mapView);
-////        getHashKey();
-//    }
-//    @RequiresApi(api = Build.VERSION_CODES.FROYO)
-//    private void getHashKey(){
-//        PackageInfo packageInfo = null;
-//        try {
-//            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-//        } catch (PackageManager.NameNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//        if (packageInfo == null)
-//            Log.e("KeyHash", "KeyHash:null");
-//
-//        for (Signature signature : packageInfo.signatures) {
-//            try {
-//                MessageDigest md = MessageDigest.getInstance("SHA");
-//                md.update(signature.toByteArray());
-//                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-//            } catch (NoSuchAlgorithmException e) {
-//                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
-//            }
-//        }
-//    }
-//
-//}
+package com.example.refresh_selection;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.ViewGroup;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import net.daum.android.map.MapViewEventListener;
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+//import net.daum.android.map.MapView;
+
+public class MapActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.FROYO)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        MapView mapView = new MapView(this);
+        //지도띄우는 화면
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+        //지도를 현재위치에 있는 장소로 띄어줌
+        mapView.setMapViewEventListener((MapViewEventListener) this);//맞나?
+        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithHeading);
+
+        //마커객체 생성
+        MapPOIItem marker = new MapPOIItem();
+
+        //맵 포인트 위도경도 설정 나중에 서버에서 제공하는 위도 ,경도로 설정
+        MapPoint mapPoint = MapPoint.mapPointWithGeoCoord(35.898054, 128.544296);
+        marker.setItemName("Default Marker");//마커이름
+        marker.setTag(0);
+        marker.setMapPoint(mapPoint);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+
+        mapView.addPOIItem(marker);//마커 표시
+
+
+    }//onCreate끝
+
+    //지도에 마커를 표시하는 함수가 있어야 나중에 서버에서 위도 경도 받아서 마커 표시 할 수 있을 듯
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.FROYO)
+    private void getHashKey(){
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageInfo == null)
+            Log.e("KeyHash", "KeyHash:null");
+
+        for (Signature signature : packageInfo.signatures) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            } catch (NoSuchAlgorithmException e) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+            }
+        }
+    }
+
+}
